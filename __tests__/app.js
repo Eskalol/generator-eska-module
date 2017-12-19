0ce31aed90903da690657464d31392d4a5ae93aa
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-12-19 18:11:25 +0100

    testing stuff

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,30 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+const defaultPrompts = {
+  moduleName: 'test stuff',
+  moduleDescription: 'some awesome description',
+  authorName: 'cool',
+  authorEmail: 'cool@example.com',
+  authorWebsite: 'superAwesome.com',
+  gitRepo: 'myAwesome/repo.git',
+  licesne: 'MIT'
+};
+
 describe('generator-eska-module:app', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+  describe('eslint:airbnb and jest', () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+        ...defaultPrompts,
+        cli: false,
+        tests: 'jest',
+        linter: 'eslint',
+        eslintConfing: 'airbnb'
+      });
+    });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['package.json']);
   });
 });
